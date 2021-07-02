2f9d62576ac56d1fbe7d9b0abd1bd3d173836c8b
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-07-02 06:03:12 -0300

    add estilos e configuração de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+
+import { GlobalStyle } from './styles/global';
 
 function App(): JSX.Element {
-  return <div className="App" />;
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
